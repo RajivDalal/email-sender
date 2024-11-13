277075eb2f28d1e03e0065e51a9b59ee5a2fe628
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2024-11-14 01:01:54 +0530

    feat: Flask Endpoint to Read Data from Google Sheets

--- email-backend/app.py
+++ email-backend/app.py
@@ -1,5 +1,6 @@
 import io
 from flask import Flask, request
+from google_sheets import read_google_sheet
 import csv
 
 app = Flask(__name__)
@@ -28,6 +29,28 @@ def upload_csv():
         return {"error": str(e)}, 500
 
     return {'message': "CSV uploaded successfully", 'data': data}, 200
+
+@app.route('/fetch_google_sheet', methods=['GET'])
+def fetch_google_sheet():
+    sheet_id = request.args.get('sheet_id')
+    range_name = request.args.get('range_name')
+
+    if not sheet_id or not range_name:
+        return {"error": "Missing sheet_id or range_name"}, 400
+    
+    try:
+        raw_data = read_google_sheet(sheet_id,range_name)
+
+        if not raw_data:
+            return {"error": "Data not Sufficient"}, 404
+
+        headers = raw_data[0]
+        data = [dict(zip(headers, row)) for row in raw_data[1:]]
+
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+    return {"message": "Data fetched Sucecsfully", "data": data}, 200
 
 if __name__ == "__main__":
     app.run(debug=True)
