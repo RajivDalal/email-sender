96e2d42bd47753a4f6d1793724a5601ee61efdc7
Rajiv Dalal <rajiv.dalal2022@vitstudent.ac.in>
2024-11-13 22:58:22 +0530

    feat: Flask Endpoint to Read Data from CSV file

--- email-backend/app.py
+++ email-backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+import csv
 
 app = Flask(__name__)
 
@@ -6,6 +7,25 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route('/upload.csv', methods=['POST'])
+def upload_csv():
+    if 'file' not in request.files:
+        return {'error': 'File not uploaded'}, 400
+
+    file = request.files['file']
+
+    if not file.filename.endswith('.csv'):
+        return {'error': 'Invalid File Type'}, 400
+
+    data = []
+    try:
+        csv_read = csv.DictReader(file.stream)
+        for row in csv_read:
+            data.append(row)
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+    return {'message': "CSV uploaded successfully", 'data': data}, 200
+
 if __name__ == "__main__":
     app.run(debug=True)
-
